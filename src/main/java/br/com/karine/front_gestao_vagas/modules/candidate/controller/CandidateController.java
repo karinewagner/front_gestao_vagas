a9d377c21cf939dc893a0590a3f4a3a2e154558c
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-08 16:49:10 -0400

    cria controller do login

---
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -0,0 +1,41 @@
+package br.com.karine.front_gestao_vagas.modules.candidate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.karine.front_gestao_vagas.modules.candidate.service.CandidateService;
+
+@Controller
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+    
+    @GetMapping("/login")
+    public String login() {
+        return "candidate/login";
+    }
+
+    @PostMapping("/signIn")
+    public String singIn(RedirectAttributes redirectAttributes, String username, String password) {
+
+        try {
+            this.candidateService.login(username, password);
+            
+            return "/candidate/profile";
+
+        } catch (HttpClientErrorException e) {
+            
+            redirectAttributes.addFlashAttribute("error_message", "Usuário/senha incorrtato.");
+            
+            return "redirect:/candidate/login";
+        }
+
+    }
+}
