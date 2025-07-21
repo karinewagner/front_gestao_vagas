df4537ad243e8f997583e67a48a7fec230cb5561
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-21 18:11:57 -0400

    inclui try/catch no profile service

--- src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/service/ProfileCandidateService.java
@@ -5,21 +5,30 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import br.com.karine.front_gestao_vagas.modules.candidate.dto.ProfileUserDTO;
 
 @Service
 public class ProfileCandidateService {
     
-    public String execute(String token) {
+    public ProfileUserDTO execute(String token) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
 
-       var result = rt.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, String.class);
+        try {
+            var result = rt.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDTO.class);
+            return result.getBody();
+        } catch (Exception e) {
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+        }
 
-       return result.getBody();
+
     }
 }
