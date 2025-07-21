cd961f79ae14c7fa6a204e9c133f621dafba6d24
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-21 18:11:16 -0400

    cria ProfileUserDTO

---
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/dto/ProfileUserDTO.java
@@ -0,0 +1,20 @@
+package br.com.karine.front_gestao_vagas.modules.candidate.dto;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileUserDTO {
+    
+    private UUID id;
+    private String username;
+    private String name;
+    private String email;
+    private String description;
+
+}
