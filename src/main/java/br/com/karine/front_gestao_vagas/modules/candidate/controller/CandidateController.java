381019055a87b6b7d9765330b4f9585addab75e1
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-31 14:34:21 -0400

    cria as rotas para listar vagas e aplicar

--- src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,5 +1,7 @@
 package br.com.karine.front_gestao_vagas.modules.candidate.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,10 +14,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.karine.front_gestao_vagas.modules.candidate.service.ApplyJobService;
 import br.com.karine.front_gestao_vagas.modules.candidate.service.CandidateService;
+import br.com.karine.front_gestao_vagas.modules.candidate.service.FindJobsService;
 import br.com.karine.front_gestao_vagas.modules.candidate.service.ProfileCandidateService;
 import jakarta.servlet.http.HttpSession;
 
@@ -28,6 +33,12 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateService profileCandidateService;
+
+    @Autowired
+    private FindJobsService findJobsService;
+
+    @Autowired
+    private ApplyJobService applyJobService;
     
     @GetMapping("/login")
     public String login() {
@@ -73,5 +84,36 @@ public class CandidateController {
             return "redirect:/candidate/login";
         }
 
+    }
+
+    @GetMapping("/jobs")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String jobs(Model model,String filter) {
+
+        try {
+            if (filter != null) {
+
+                var jobs = this.findJobsService.execute(getToken(), filter);
+                model.addAttribute("jobs", jobs);
+            }
+        } catch (HttpClientErrorException e) {
+            return "redirect:/candidate/login";
+        }
+        return "candidate/jobs";
+    }
+
+    @PostMapping("/job/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String applyJob(@RequestParam("jobId") UUID jobId) {
+        System.out.println("Applying for job with ID: " + jobId);
+
+        this.applyJobService.execute(getToken(), jobId);
+
+        return "redirect:/candidate/jobs";
+    }
+
+    private String getToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getDetails().toString();
     }
 }
