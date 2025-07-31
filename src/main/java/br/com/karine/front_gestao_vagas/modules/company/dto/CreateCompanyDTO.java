ec32871049395a6b8699485c608a3778c6cf1b3f
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-31 18:08:54 -0400

    cria o CreateCompanyDTO

---
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/company/dto/CreateCompanyDTO.java
@@ -0,0 +1,11 @@
+package br.com.karine.front_gestao_vagas.modules.company.dto;
+
+public class CreateCompanyDTO {
+    
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+    private String website;
+    private String description;
+}
