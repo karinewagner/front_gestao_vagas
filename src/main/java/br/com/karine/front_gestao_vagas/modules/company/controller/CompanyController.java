5c8ac748c170495aad58abd6bffeeaa7d819951a
Karine Débora Wagner <karinedwagner@gmail.com>
2025-08-01 16:32:57 -0400

    inclui a rota de login da company

--- src/main/java/br/com/karine/front_gestao_vagas/modules/company/controller/CompanyController.java
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/company/controller/CompanyController.java
@@ -1,39 +1,80 @@
 package br.com.karine.front_gestao_vagas.modules.company.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.karine.front_gestao_vagas.modules.company.dto.CreateCompanyDTO;
+import br.com.karine.front_gestao_vagas.modules.company.service.CompanyService;
 import br.com.karine.front_gestao_vagas.modules.company.service.CreateCompanyService;
 import br.com.karine.front_gestao_vagas.utils.FormatErrorMessage;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/company")
 public class CompanyController {
 
     @Autowired
+    private CompanyService companyService;
+
+    @Autowired
     private CreateCompanyService createCompanyService;
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "company/login";
+    }
+
+    @PostMapping("/signIn")
+    public String singIn(RedirectAttributes redirectAttributes, HttpSession session, String username, String password) {
+
+        try {
+            var token = this.companyService.login(username, password);
+            var grants = token.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(null, null, grants);
+            auth.setDetails(token.getAccess_token());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+            session.setAttribute("token", token);
+            
+            return "redirect:/company/jobs";
+
+        } catch (HttpClientErrorException e) {
+            
+            redirectAttributes.addFlashAttribute("error_message", "Usuário/senha incorreta.");
+            
+            return "redirect:/company/login";
+        }
+    }
 
     @GetMapping("/create")
     public String create(Model model) {
-        model.addAttribute("createCompanyDTO", new CreateCompanyDTO());
+        model.addAttribute("company", new CreateCompanyDTO());
         return "company/create";
     }
 
     @PostMapping("/create")
-    public String save(Model model, CreateCompanyDTO createCompanyDTO) {
+    public String save(Model model, CreateCompanyDTO company) {
         try {
-            this.createCompanyService.execute(createCompanyDTO);
+            this.createCompanyService.execute(company);
         } catch (HttpClientErrorException e) {
-            model.addAttribute("error_message", FormatErrorMessage.formatErrorMessage(e.getResponseBodyAsString()));
+            model.addAttribute("error_message", 
+                FormatErrorMessage.formatErrorMessage(e.getResponseBodyAsString()));
+            model.addAttribute("company", company);
+            return "company/create";
         }
-
-        model.addAttribute("company", createCompanyDTO);
 
         return "company/login";
     }
