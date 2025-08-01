d585ff3fd157fa64c356dbf25d4d6a96106f5b3c
Karine Débora Wagner <karinedwagner@gmail.com>
2025-08-01 16:32:03 -0400

    cria o CreateCompanyDTO

--- src/main/java/br/com/karine/front_gestao_vagas/modules/company/dto/CreateCompanyDTO.java
+++ src/main/java/br/com/karine/front_gestao_vagas/modules/company/dto/CreateCompanyDTO.java
@@ -1,5 +1,12 @@
 package br.com.karine.front_gestao_vagas.modules.company.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateCompanyDTO {
     
     private String username;
